11bc7f57920824cd9ced0a93f5c7fdbd4ebe2bae
justinzhong7789 <justinzhong7789@gmail.com>
2021-01-11 18:23:35 -0800

    Finished implementation of gravity

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public CharacterController controller;  
-    public float speed = 10f;
+    public float WalkingSpeed = 10f;
+    public float GRAVITATIONAL_ACCELERATION = -9.18f;
+    public float fallSpeed = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         
     }
 
@@ -17,8 +20,15 @@ public class movement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        if(controller.isGrounded){
+            fallSpeed = 0f;
+        }
+        else{
+            fallSpeed += GRAVITATIONAL_ACCELERATION * Time.deltaTime;
+        }
+        Vector3 move = transform.right * x * WalkingSpeed + transform.forward * z * WalkingSpeed
+                       + transform.up * fallSpeed ;
+        controller.Move(move*Time.deltaTime);
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move*speed*Time.deltaTime);
     }
 }
